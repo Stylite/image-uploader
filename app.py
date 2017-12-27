393af0ad78316f12da1c6da4f77f852c149e6f1f
Devoxin <lukel9914@gmail.com>
2017-12-27 21:03:43 +0000

    Add more configurables, flake8

--- app.py
+++ app.py
@@ -5,9 +5,15 @@ import re
 
 from flask import Flask, render_template, request
 
-## Configurable Options ##
+# Configurable Options
+debug = False
+include_port = False
 storage = os.path.basename('files')
-domain = 'http://localhost:8080'  # Include subdomain if required
+port = 8080
+domain = 'http://localhost'  # Include subdomain if required
+
+if debug or include_port:
+    domain += f':{port}'
 
 app = Flask(__name__)
 extension_regex = re.compile("\.[a-z0-9]+$", re.IGNORECASE)
@@ -17,6 +23,7 @@ extension_regex = re.compile("\.[a-z0-9]+$", re.IGNORECASE)
 def index():
     return render_template('index.html')
 
+
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'GET':
@@ -25,7 +32,7 @@ def upload_file():
         attachment = request.files.get('image', None)
 
         if not attachment:
-            return json.dumps({ 'status': 400, 'error': 'Missing file "attachment"'}), 200
+            return json.dumps({'status': 400, 'error': 'Missing file "attachment"'}), 200
 
         extension = extension_regex.match(attachment.filename)
 
@@ -34,11 +41,15 @@ def upload_file():
         file_path = os.path.join(storage, file_name)
         attachment.save(file_path)
 
-        return json.dumps({ 'status': 200, 'url': f'{domain}/{file_name}' }), 200
+        return json.dumps({'status': 200, 'url': f'{domain}/{file_name}'}), 200
 
 
 def generate_hex(length=10):  # Defaults to 10
     return f"%0{length}x" % random.randrange(16**length)
 
 
-app.run(port=8080)
+if __name__ == '__main__':
+    if debug:
+        app.run(port=port)
+    else:
+        app.run(host='0.0.0.0', port=port)
